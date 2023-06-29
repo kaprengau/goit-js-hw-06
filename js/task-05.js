28e92dd2cd409dbd25d03955d2c7b2cce8e4cd54
kaprengau <131276622+kaprengau@users.noreply.github.com>
2023-06-29 10:31:13 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -8,8 +8,8 @@ const nameInput = document.querySelector("#name-input");
 const nameOutput = document.querySelector("#name-output");
 
 nameInput.addEventListener("input", ({target}) => {
-  const name = nameInput.value.trim();
-  if (name.target === "") {
+  const name = target.value.trim();
+  if (name === "") {
     nameOutput.textContent = "Anonymous";
   } else {
     nameOutput.textContent = name;
